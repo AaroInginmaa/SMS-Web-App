b2f5eaba06b693f36dd6e18f6e3809cfe4b57b83
AaroInginmaa <aaro.inginmaa@gmail.com>
2023-11-15 09:44:22 +0200

    Added intercat.js

---
+++ public/interact.js
@@ -0,0 +1,8 @@
+function GetValues() {
+    let phone = document.getElementById('phone').value;
+    let msg = document.getElementById('msg').value; 
+
+    console.log("done");
+    return phone, msg;
+
+}
