651391a67ecef86ca52c2a50b5f4735d551c2b62
AaroInginmaa <aaro.inginmaa@gmail.com>
2023-11-15 11:09:23 +0200

    updates

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const fs = require('fs');
+
 const app = express();
 const port = 8000;
 
@@ -11,3 +13,31 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Server up on port ${port}`)
 })
+
+function makeid(length) {
+    let result = '';
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    const charactersLength = characters.length;
+    let counter = 0;
+    while (counter < length) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+      counter += 1;
+    }
+    return result;
+}
+
+function sendsms() {
+  const path = require('path');
+  
+  const getRootDir = () => path.parse(process.cwd()).root;
+  console.log(getRootDir());
+  
+  
+  let filepath = getRootDir() + "/var/spool/sms/outgoing/"
+  let filename = makeid(8);
+  let filecontents = "To: 0443642653\nFlash: yes\nAlphabet: ISO\n Test message";
+  
+  console.log(filename);
+  
+  fs.writeFile(filepath + filename, filecontents);
+}
--- public/interact.js
+++ public/interact.js
@@ -1,6 +1,21 @@
 function getValues() {
     let phone = document.getElementById('phone').value;
-    let msg = document.getElementById('msg').value; 
-
-    
+    let msg = document.getElementById('msg').value;    
 }
+
+function sendsms() {
+    const path = require('path');
+    
+    const getRootDir = () => path.parse(process.cwd()).root;
+    console.log(getRootDir());
+    
+    
+    let filepath = getRootDir() + "/var/spool/sms/outgoing/"
+    let filename = makeid(8);
+    let filecontents = "To: ${phone}\nFlash: yes\nAlphabet: ISO\n ${msg}";
+    
+    console.log(filename);
+    
+    fs.writeFile(filepath + filename, filecontents);
+  }
+  
