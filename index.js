71c2370c6a50d858b5276fe11f1f0396e23d82a2
AaroInginmaa <aaro.inginmaa@gmail.com>
2023-11-27 11:49:30 +0200

    Removed unnecessary code

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const fs = require('fs');
 const bodyParser = require('body-parser');
-const { format } = require('path');
 
 const app = express();
 const port = 80;
