41579680923ea06bf940ab491f36cfab25562d73
AaroInginmaa <aaro.inginmaa@gmail.com>
2023-12-05 13:07:37 +0200

    readability fix

--- public/responseHandler.js
+++ public/responseHandler.js
@@ -1,5 +1,6 @@
-let node = document.createElement("p");
+let node;
 function createElement() {
+    node = document.createElement("p");
     document.getElementById("status-message").appendChild(node);
     node.textContent = 'Sending message';
     node.setAttribute("class", "p-3 bg-warning bg-opacity-10 border border-warning rounded text-black");
@@ -7,11 +8,11 @@ function createElement() {
 
 function handleResponse(result) {
     if (result['error']) {
-        node.textContent = result['error']
+        node.textContent = result['error'];
         node.setAttribute("class", "p-3 bg-danger bg-opacity-10 border border-danger rounded text-black");
     }
     else {
-        node.textContent = result['result']
+        node.textContent = result['result'];
         node.setAttribute("class", "p-3 bg-success bg-opacity-10 border border-success rounded text-black");
     }
 }
